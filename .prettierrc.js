0904f7408e55266d3a2cb8e5b33df42429c83a26
Rafael Santos <rafa.05125@gmail.com>
2023-07-27 06:41:23 -0300

    Eslint, Prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true,
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: false,
+  trailingComma: "all",
+  arrowParens: "avoid",
+  printWitdh: 80,
+};
