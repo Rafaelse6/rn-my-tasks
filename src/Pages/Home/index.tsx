be607f4d8b75ef907b685e44efcae442a733922f
Rafael Santos <rafa.05125@gmail.com>
2023-07-27 22:39:09 -0300

    SafeAreaView component

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,21 +1,30 @@
 import {Text, View, StyleSheet} from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context";
 
 export const Home = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Olá Dev!</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Welcome,Dev!</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#121214",
+  },
   container: {
     flex: 1,
     backgroundColor: "#121214",
-    justifyContent: "center",
-    alignItems: "center",
+    paddingHorizontal: 30,
+    paddingVertical: 50,
   },
   title: {
     color: "#f1f1f1",
+    fontSize: 24,
+    fontWeight: "bold",
   },
 });
