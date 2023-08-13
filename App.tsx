1b0a97352f00dd3f8fc361f46d72ef976728b541
Rafael Santos <rafa.05125@gmail.com>
2023-08-13 18:01:07 -0300

    TasksContext refactoring

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import {Home} from "./src/Pages/Home";
-import {TasksContext} from "./src/context/TasksContext";
+import {TasksProvider} from "./src/context/TasksContext";
 
 export default function App() {
   return (
-    <TasksContext.Provider value={{id: "1", title: "Task01"}}>
+    <TasksProvider>
       <Home />
-    </TasksContext.Provider>
+    </TasksProvider>
   );
 }
